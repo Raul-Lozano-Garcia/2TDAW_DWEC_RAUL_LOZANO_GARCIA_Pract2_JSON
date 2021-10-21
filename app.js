5cf002771b6e3a14a02db2ef6dcb2680fdc88fb0
Raúl Lozano García <59658847+Raul-Lozano-Garcia@users.noreply.github.com>
2021-10-21 10:52:07 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,121 @@
+//¿INFO DEL ARTICLE ASÍ A PALO SECO, CON LOS TRUE Y TODO?
+
+"use strict"
+
+let ordenar=prompt("¿Como quieres mostrar los datos ordenados por nombre. ¿De manera ascendente (por defecto) o descendente?") || "ascendente";
+let categoria=prompt("¿Por qué tipo de carne lo quieres filtrar? (whopper, burger o pollo) (dejar vacío si no se quiere filtrar)");
+document.write(`
+<header>
+    <div class="logo">
+        <img src="https://1000marcas.net/wp-content/uploads/2019/12/Logo-Burger-King.png" alt="logo">
+    </div>
+    <h1>Lista de hamburguesas</h1>
+</header>
+`);
+
+let cont=1;
+let fecha;
+
+//SI EL USUARIO ESCRIBE COSAS SIN SENTIDO O NO ESCRIBE NADA SE MOSTRARÁN TAL Y COMO ESTÁN EN EL JSON
+if(ordenar.toLocaleLowerCase()==="ascendente"){
+    hamburguesas.sort(
+        (a,b)=>{
+            return a["nombre"].localeCompare(b["nombre"]);
+        });
+}else if(ordenar.toLocaleLowerCase()==="descendente"){
+    hamburguesas.sort(
+        (a,b)=>{
+            return b["nombre"].localeCompare(a["nombre"]);
+        });
+}else{
+    alert("No has introducido un orden válido, por lo que se mostrarán las hamburguesas sin orden");
+}
+
+let ham_filtradas=hamburguesas.filter(
+    (item)=>{
+        return item["tipo de carne"].includes(categoria.toLowerCase());
+    }
+);
+
+if(ham_filtradas.length===0){
+    alert("No has introducido un tipo de carne válido, por lo que no se mostrarán hamburguesas");
+}
+
+ham_filtradas.forEach(
+    (item)=>{
+        fecha=item["fecha de creacion"].split("-");
+        document.write(`
+        <main>
+            <section>
+                <article>
+                    <div class="burger">
+                        <img src="${item["imagen"]}" alt="hamburguesa${cont}">
+                    </div>
+                    <h2>${item["nombre"]}</h2>
+                    <p>${item["precio"]}€</p>
+                    <p>${item["ingredientes"]}</p>
+                    <p>${item["peso"]}g</p>
+                    <p>${item["tipo de carne"]}</p>
+                    `);
+                    switch (item["lechuga"]) {
+                        case true:
+                            document.write("<p>Con lechuga</p>");
+                            break;
+                    
+                        case false:
+                            document.write("<p>Sin lechuga</p>");
+                            break;
+                    }
+                    document.write(`
+                    <p>${fecha[2]}/${fecha[1]}/${fecha[0]}</p>
+                    <a href="${item["pagina del producto"]}" target="_blank">Página del producto</a>
+                </article>
+                <form action="#" method="post">
+                    <fieldset>
+                        <legend>Actualizar datos de ${item["nombre"]}</legend>
+                        <div>
+                            <label for="nombre${cont}">Nombre</label>
+                            <input type="text" name="nombre${cont}" id="nombre${cont}" value="${item["nombre"]}">
+                        </div>   
+                        <div>
+                            <label for="precio${cont}">Precio</label>
+                            <input type="number" name="precio${cont}" id="precio${cont}" value="${item["precio"]}">
+                        </div>    
+                        <div>
+                            <label for="peso${cont}">Peso</label>
+                            <input type="number" name="peso${cont}" id="peso${cont}" value="${item["peso"]}">
+                        </div>  
+                        
+                        <div>
+                            <label for="carne${cont}">Tipo de carne</label>
+                            <input type="text" name="carne${cont}" id="carne${cont}" value="${item["tipo de carne"]}">
+                        </div>
+                        <div>
+                            <label for="lechuga${cont}">¿Lleva lechuga?</label>
+                            <input type="text" name="lechuga${cont}" id="lechuga${cont}" value="${item["lechuga"]}">
+                        </div>      
+                        <div>
+                            <label for="fecha${cont}">Fecha de creación</label>
+                            <input type="date" name="fecha${cont}" id="fecha${cont}" value="${item["fecha de creacion"]}">
+                        </div>       
+                        <div>
+                            <label for="imagen${cont}">URL imagen</label>
+                            <input type="text" name="imagen${cont}" id="imagen${cont}" value="${item["imagen"]}">
+                        </div>    
+                        <div>
+                            <label for="pagina${cont}">URL pagina del producto</label>
+                            <input type="text" name="pagina${cont}" id="pagina${cont}" value="${item["pagina del producto"]}">
+                        </div>  
+                        <div>
+                            <label for="ingredientes${cont}">Ingredientes</label>
+                            <textarea name="ingredientes${cont}" rows="3" id="ingredientes${cont}">${item["ingredientes"]}</textarea>
+                        </div>        
+                        <input type="submit" name="enviar${cont}" value="Enviar modificaciones">         
+                    </fieldset>
+                </form>
+            </section>
+        </main>
+        `);
+        cont++;
+    }
+)
---
+++ datos.js
@@ -0,0 +1,59 @@
+"use strict"
+
+const hamburguesas=[
+    {
+        "precio":8,
+        "imagen":"https://gps.burgerkingencasa.es/images/products/1576063004187_Doble_Cheese_Bacon_XXL.png",
+        "nombre":"Double cheese bacon XXL",
+        "ingredientes":"Carne whopper, bacon, mostaza, ketchup, queso y pepinillo",
+        "peso":333.6,
+        "tipo de carne":"whopper",
+        "pagina del producto":"https://www.burgerking.es/carta/menus/hamburguesas/doble-cheese-bacon-xxl",
+        "lechuga":false,
+        "fecha de creacion":"2015-05-01"
+    },
+    {
+        "precio":6,
+        "imagen":"https://gps.burgerkingencasa.es/images/products/1633605736725_ss--long-vegetal-dest-esp.png",
+        "nombre":"Long vegetal",
+        "ingredientes":"Pollo, lechuga y mayonesa",
+        "peso":217.2,
+        "tipo de carne":"pollo",
+        "pagina del producto":"https://www.burgerking.es/carta/menus/hamburguesas/long-vegetal",
+        "lechuga":true,
+        "fecha de creacion":"2016-07-12"
+    },
+    {
+        "precio":10,
+        "imagen":"https://gps.burgerkingencasa.es/images/products/1575463631926_Menu_Big_King.png",
+        "nombre":"Big king",
+        "ingredientes":"Carne burger, lechuga, queso, pepinillo, salsa big king y cebolla",
+        "peso":224.1,
+        "tipo de carne":"burger",
+        "pagina del producto":"https://www.burgerking.es/carta/menus/menu-parrilla/menu-big-king",
+        "lechuga":true,
+        "fecha de creacion":"2013-01-23"
+    },
+    {
+        "precio":9,
+        "imagen":"https://gps.burgerkingencasa.es/images/products/1575530622161_Menu_Crispy_Chicken.png",
+        "nombre":"Crispy chicken",
+        "ingredientes":"Pollo, lechuga, tomate y mayonesa",
+        "peso":189.1,
+        "tipo de carne":"pollo",
+        "pagina del producto":"https://www.burgerking.es/carta/menus/menu-pollo/menu-crispy-chicken",
+        "lechuga":true,
+        "fecha de creacion":"2015-12-11"
+    },
+    {
+        "precio":13,
+        "imagen":"https://gps.burgerkingencasa.es/images/products/1575463162266_Menu_The_King_Bacon(2_carnes).png",
+        "nombre":"The king bacon",
+        "ingredientes":"Carne whopper, queso, bacon, tomate y mayonesa de bacon",
+        "peso":341.6,
+        "tipo de carne":"whopper",
+        "pagina del producto":"https://www.burgerking.es/carta/menus/menu-parrilla/menu-the-king-bacon",
+        "lechuga":false,
+        "fecha de creacion":"2017-02-16"
+    }
+];
